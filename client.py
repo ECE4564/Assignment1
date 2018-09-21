a831e18df64688b2afe0cd4e4ec4ffed073aa8f0
Samuel Giacinto <samg5@vt.edu>
2018-09-21 10:42:47 -0400

    Should be good, may be a bit rough, but ready to test/intergrate?

--- client.py
+++ client.py
@@ -1,17 +1,28 @@
-from src.clientargparser import client_parser
+from clientImport import *
 
-from clientVariable import key
+def deconstruct(Data):
+    return pickle.loads(Data)
 
-from clientVariable import consumer_key
-from clientVariable import consumer_secret
+def md5IsValid(encryptedAnswer,md5):
+    temp = hashlib.md5(encryptedAnswer).hexdigest()
+    print(temp)
+    return ( md5 == temp )
 
-from clientVariable import access_token
-from clientVariable import access_token_secret
+def assemblePayload(answer):
+    # step1. generate a key for encryption
+    newKey = Fernet.generate_key()
+    f = Fernet(newKey)
+    #step2. Encrypt the answer
+    encyptedAnswer = f.encrypt(answer.encode())
+    #step3. Generate CheckSum for the answer
+    checkSum = hash(encyptedAnswer)
 
-import os
-import tweepy
-from tweepy import Stream
-from tweepy.streaming import StreamListener
+    return pickle.dumps([newKey,encyptedAnswer,checkSum])
+
+def decrypt(question,key):
+    f = Fernet(key)
+    decryptedQuestion = f.decrypt(question)
+    return decryptedQuestion
 
 #override tweepy.StreamListener to add logic to on_status
 class filterListener(StreamListener):
@@ -27,16 +38,55 @@ class filterListener(StreamListener):
             except AttributeError:
                 tweet = status.text
 
-        stripped_tweet = tweet.replace(hash_tag, '')
+        #assemble stripped tweet payload
+        payload = assemblePayload(tweet.replace(hash_tag, ''))
 
-        print(tweet)
+        print("C1")
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        print("C2")
+
+        s.bind((bridge_ip, bridge_port))
+
+        print("C3")
+
+        s.listen(socket_size)
+
+        print("C4")
+
+        bridge, address = s.accept()
+
+        print("C5")
+
+        bridge.send(payload)
+
+        print("C6")
+
+        data = bridge.recv(socket_size)
+
+        print("C7")
+
+        key,encryptedAnswer,checkSum = deconstruct(data)
+
+        print("D")
+
+        if data:
+            if md5IsValid(encryptedAnswer,checkSum):
+                decryptedAnswer = (decrypt(encryptedAnswer,key)).decode("utf-8")
+                print(decryptedAnswer)
+            else:
+                print("Invalid CheckSum")
+                sys.exit(0)
+
+        print("E")
         return True
 
 args = client_parser().parse_args()
 
 bridge_ip = args.bridge_ip
-bridge_port = args.bridge_port
-socket_size = args.socket_size
+bridge_port = int(args.bridge_port)
+socket_size = int(args.socket_size)
 hash_tag = args.hash_tag
 
 print(bridge_ip, bridge_port, socket_size, hash_tag)
---
+++ clientImport.py
@@ -0,0 +1,10 @@
+import os
+import tweepy
+import pickle
+import socket,sys,getopt
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+from src.clientargparser import client_parser
+
+from clientVariable import *
