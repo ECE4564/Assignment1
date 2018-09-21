8ad5fc443fffc485231a40d75f3d4d19130ed600
Samuel Giacinto <samg5@vt.edu>
2018-09-20 21:49:24 -0400

    Reading in tweets, god I hate people..

--- client.py
+++ client.py
@@ -1,5 +1,33 @@
 from src.clientargparser import client_parser
 
+from clientVariable import consumer_key
+from clientVariable import consumer_secret
+
+from clientVariable import access_token
+from clientVariable import access_token_secret
+
+import os
+import tweepy
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+#override tweepy.StreamListener to add logic to on_status
+class filterListener(StreamListener):
+    def on_status(self, status):
+        if hasattr(status, 'retweeted_status'):
+            try:
+                tweet = status.retweeted_status.extended_tweet["full_text"]
+            except:
+                tweet = status.retweeted_status.text
+        else:
+            try:
+                tweet = status.extended_tweet["full_text"]
+            except AttributeError:
+                tweet = status.text
+
+        print(tweet)
+        return True
+
 args = client_parser().parse_args()
 
 bridge_ip = args.bridge_ip
@@ -8,3 +36,13 @@ socket_size = args.socket_size
 hash_tag = args.hash_tag
 
 print(bridge_ip, bridge_port, socket_size, hash_tag)
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.secure = True
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+customListener = filterListener()
+customStream = Stream(api.auth, customListener)
+customStream.filter(track=['#fakenews'])
---
+++ clientVariable.py
@@ -0,0 +1,5 @@
+consumer_key ="x0Wi5SlI04srhHj3eitAK5arK"
+consumer_secret="gFAEh56WadqykG4KcvvjB46sXHBRb37JdT6fOjHF3EPsagXuIV"
+
+access_token="2550582097-ke3zdtjCTh3RIa2ngNUlHGJ8otKhBghFlbYUcS5"
+access_token_secret="HBcDvOTBEft0HBS4xPVdWSzuGSy8LrOz44BjVafAil6qI"
